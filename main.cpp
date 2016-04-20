0a11d2373a95c9632045144121e39b1ece00fcf1
Tad-Done <tjy140896@163.com>
2016-04-20 14:27:15 +0800

    sample

--- main.cpp
+++ main.cpp
@@ -13,22 +13,7 @@ int Legendre(BigInt a,BigInt p)
 
 int main()
 {
-    BigInt a=-125;
-    //c=c*a*a;
-    //cout<<c.modOfPower(1,b)<<endl;
-    cout<<a<<endl;
-    //b=a.power(4)+7*a+4;
-    //b=20140515*a.power(3)+201495*a.power(15)+2014*a.power(40)+8*a.power(8)+a.power(6)+4*a.power(3)+a+1;
-    //b=2014*a.power(4)+20342014*a.power(3)+8*a*a+a+2;
-    //c=c.power(42);
-    //c=42*a.power(13)+169*a.power(12)+150*a.power(9)+18*a.power(8)+6*a.power(5)+15*a.power(2);
-    /*
-    for(a=0;a<343;a=a+1){
-        b=20140515*a.power(45)+201495*a.power(15)+2014*a.power(250)+8*a.power(8)+a.power(6)+4*a.power(3)+a+1;
-        if(b%343==0)cout<<a<<endl;
-    }
-    */
-
-    //c=c.power(294);
+    BigInt a=1245,b=1823;
+    bezout(a,b);
     return 0;
 }
